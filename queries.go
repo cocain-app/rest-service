a1278d041de6eeba3b46f2e9d4588fe8e31fb91d
m-lukas <lukas.mueller.gr@t-online.de>
2018-10-23 17:07:05 +0200

    Update suggestion query

--- queries.go
+++ queries.go
@@ -3,8 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
 
 	_ "github.com/lib/pq"
@@ -12,47 +10,31 @@ import (
 
 func getSuggestions(w http.ResponseWriter, r *http.Request) {
 
-	songTitle := "Losing It"
+	songTitle := r.Header.Get("songTitle")
 	db := initDB()
 	defer db.Close()
-	songID, err := getSongID(songTitle, db) //err = no results
-	checkErr(err, "No query results")
-	transitions, err := getTransitions(songID, db) //err= no results
-	checkErr(err, "No query results")
+	rows, err := db.Query("select songs.id as id, songs.title as title, artists.name as artistName, count(songs.id) as score from songs join artists on artists.id = songs.artist_id "+
+		"join transitions on transitions.song_to=songs.id where transitions.song_from = (select id from songs where songs.title like $1 limit 1) "+
+		"group by songs.id, artists.id order by score desc", songTitle)
+	checkErr(err, "Query error!")
 
-	fmt.Println(transitions)
+	var suggestions []Song
 
-	json.NewEncoder(w).Encode(transitions)
+	for rows.Next() {
+		var id sql.NullString
+		var title sql.NullString
+		var artistName sql.NullString
+		var score sql.NullInt64
+		err = rows.Scan(&id, &title, &artistName, &score)
+		checkErr(err, "Corrupt data format!")
 
-	/*
-		rows, err := db.Query("SELECT id, title, artist_id, release_date, duration, spotify_uri, timestamp_added, timestamp_modified FROM public.songs WHERE title=$1", songTitle)
-			checkErr(err, "Query error!")
+		suggestions = append(suggestions, Song{ID: id.String, Title: title.String, Artist: artistName.String, NumberOfSets: score.Int64})
+	}
 
-			var suggestions []Suggestion
-
-
-			for rows.Next() {
-				var id sql.NullString
-				var title sql.NullString
-				var artist_id sql.NullString
-				var release_date sql.NullString
-				var duration sql.NullInt64
-				var spotify_uri sql.NullString
-				var timestamp_added sql.NullString
-				var timestamp_modified sql.NullString
-				err = rows.Scan(&id, &title, &artist_id, &release_date, &duration, &spotify_uri, &timestamp_added, &timestamp_modified)
-				checkErr(err, "Corrupt data format!")
-				//fmt.Println(" id | title | artist_id | release_date | duration | spotify_uri | timestampt_added | timestamp_modified ")
-				fmt.Println(id, title, artist_id, release_date, duration, spotify_uri, timestamp_added, timestamp_modified)
-
-				suggestions = append(suggestions, Suggestion{ID: id.String, Title: title.String})
-			}
-
-			fmt.Println(suggestions)
-
-	*/
+	json.NewEncoder(w).Encode(suggestions)
 }
 
+/*
 func getSongID(songTitle string, db *sql.DB) (songID string, err error) {
 	rows, err := db.Query("SELECT id FROM public.songs WHERE title=$1", songTitle)
 	checkErr(err, "2: Query error!")
@@ -88,3 +70,22 @@ func getTransitions(songID string, db *sql.DB) (transitions []string, err error)
 	}
 	return
 }
+
+func getArtist(artistID string, db *sql.DB) (artistName string, err error) {
+	rows, err := db.Query("SELECT name FROM public.artists WHERE id=$1", artistID)
+	checkErr(err, "4: Query error!")
+
+	for rows.Next() {
+		var name sql.NullString
+		err = rows.Scan(&name)
+		checkErr(err, "Corrupt data format!")
+
+		artistName = name.String //Catch multiple ids
+	}
+
+	if artistID == "" {
+		err = errors.New("sql error: no query results")
+	}
+	return
+}
+*/
---
+++ song.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "github.com/lib/pq"
+)
+
+//Song object for returning songlist
+type Song struct {
+	ID           string `json:"ID,omitempty"`
+	Title        string `json:"Title,omitempty"`
+	Artist       string `json:"Artist,omitempty"`
+	NumberOfSets int64  `json:"NumberOfSets,omitempty"`
+}
